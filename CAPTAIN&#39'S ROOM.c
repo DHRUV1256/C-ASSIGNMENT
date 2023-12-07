c4978eca897fba55d6f385839774cd1d9c2a5f22
Dhruv Vijay Singh <147720996+DHRUV1256@users.noreply.github.com>
2023-12-08 00:37:12 +0530

    Add files via upload

---
+++ CAPTAIN&#39'S ROOM.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int findCaptainsRoom(int k, int *roomNumbers, int size) {
+    int xorResult = 0;
+
+    for (int i = 0; i < size; i++) {
+        xorResult ^= roomNumbers[i];
+    }
+
+    return xorResult;
+}
+
+int main() {
+    // Input
+    int k;
+    printf("Enter the size of each group (K): ");
+    scanf("%d", &k);
+
+    printf("Enter the room numbers (separated by space): ");
+    
+    // Assuming room numbers are entered with space separation
+    int maxSize = 1000; // You can adjust this based on the maximum number of tourists
+    int roomNumbers[maxSize];
+
+    int currentSize = 0;
+    while (scanf("%d", &roomNumbers[currentSize]) == 1) {
+        currentSize++;
+    }
+
+    // Output
+    int captainsRoom = findCaptainsRoom(k, roomNumbers, currentSize);
+    printf("Captain&#39's room number: %d\n", captainsRoom);
+
+    return 0;
+}
